842c2e3a2af7d35e42fcf41e2951c15e327c0022
Your Name <you@example.com>
2024-08-19 01:13:18 +0600

    all complate

---
+++ javascript/index.js
@@ -0,0 +1,62 @@
+function play() {
+    document.getElementById('first').classList.add('hidden')
+    document.getElementById('second').classList.remove('hidden')
+    
+    randomNumber()
+}
+
+// random number section 
+function randomNumber() {
+    const word = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '/'];
+
+    const randomNum = Math.floor(Math.random() * 27);
+
+    document.getElementById('word').innerText = word[randomNum];
+    // console.log( word[randomNum]);
+    
+    
+}
+
+// keyboard typing section 
+function clicked(allWord) {
+    const word = document.getElementById('word').innerText;
+    if (word === allWord) {
+        const score = document.getElementById('score').innerText;
+        const scoreNum = parseInt(score);
+        const totalScore = scoreNum + 10;
+
+        document.getElementById('score').innerText = totalScore;
+
+        randomNumber()
+    } else {
+        const life = document.getElementById('life').innerText;
+        const lifeNum = parseInt(life);
+        const totalLife = lifeNum - 1;
+        document.getElementById('life').innerText = totalLife;
+
+        randomNumber()
+
+        if (totalLife === 0) {
+            document.getElementById('second').classList.add('hidden');
+            document.getElementById('third').classList.remove('hidden');
+
+            const score = document.getElementById('score').innerText;
+            document.getElementById('new-score').innerText = score;
+        }
+    }
+
+}
+// play section 
+function playAgain() {
+    document.getElementById('third').classList.add('hidden');
+    document.getElementById('second').classList.remove('hidden');
+    document.getElementById('life').innerText = 5;
+
+    randomNumber()
+}
+
+
+
+
+
+// enter key press section 
